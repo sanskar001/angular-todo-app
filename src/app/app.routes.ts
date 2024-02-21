bf75e7796d5374b6776f3222866f90ab8fa6e88d
sanskar001 <sanskarm001@gmail.com>
2024-02-21 23:33:12 +0530

    FEAT: add about page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodosComponent } from './MyComponent/todos/todos.component';
+import { AboutComponent } from './MyPages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'about', component: AboutComponent },
+];
