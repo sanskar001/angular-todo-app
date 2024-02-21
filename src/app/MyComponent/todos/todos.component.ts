b88178e63c55cb48437e83279bca7ee1e811ff57
sanskar001 <sanskarm001@gmail.com>
2024-02-21 20:16:46 +0530

    FEAT: add todos component

---
+++ src/app/MyComponent/todos/todos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css',
+})
+export class TodosComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './MyComponent/todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'todo app';
+
+  constructor() {}
+
+  // Updating title after 2 seconds
+
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.title = 'title changed';
+  //   }, 2000);
+  // }
 }
